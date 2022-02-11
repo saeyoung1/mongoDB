f0904a59a7733db611c357f525ef45738d67fb36
안세영 <saeyoung.ahn@myvtw.co.kr>
2022-02-11 15:36:02 +0900

    페이지에서 라이브러리 못불러오는 오류 수정 및 몽고디비 재설정

--- src/main/java/com/asy/mongo/document/Person.java
+++ src/main/java/com/asy/mongo/document/Person.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
 @Getter
-@Document("person_info")
+@Document("person_data")
 public class Person {
     @Id
     private long registerNumber;
